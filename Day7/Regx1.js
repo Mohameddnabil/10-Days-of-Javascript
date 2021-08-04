6ca4c01e58ebbdc76625b3e43eb4c38989d36c8f
MohamedNabil <mohamed927nabil@gmail.com>
2021-08-04 15:42:13 +0200

    Day 7

---
+++ Day7/Regx1.js
@@ -0,0 +1,40 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    const re = new RegExp('(^[a].*[a]$|^[e].*[e]$|^[i].*[i]$|^[o].*[o]$'
+    + '|^[u].*[u]$)','ig');
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+     * followed by one or more letters.
+     */
+     const re = /^(Mr|Mrs|Ms|Dr|Er)[.][a-zA-Z]+$/;
+
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+
+     const re = /[0-9]+/g
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
